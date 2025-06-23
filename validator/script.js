b3e7f6c5a3da0006569a5d183aa4e6094bfd7a8d
PjZuniega92 <pjzuniega111@gmail.com>
2025-06-23 14:22:09 +0800

    another modification

--- validator/script.js
+++ validator/script.js
@@ -1,14 +1,24 @@
 const validateBtn = document.querySelector("#validate");
 validateBtn.onclick = () => {
-  validating();
+  validatingSecondVersion();
 };
 
-function validating() {
-  let age = 19;
-  let register = true;
+let age = 25;
+let register = true;
 
+function validating() {
   if (age >= 18 && register) alert("You are a valid voter. You may vote now!");
   else if (!register && age >= 18) alert("Register First, please.");
   else if (register && age < 18) alert("Invalid voter.");
   else alert("Non voter.");
+}
+
+function validatingSecondVersion() {
+  if (register) {
+    if (age >= 18) alert("You may vote now!");
+    else alert("Invalid voter!");
+  } else {
+    if (age >= 18) alert("Register first, please.");
+    else alert("Non voter!");
+  }
 }
